5c34dd97a64246b4ba3b9057998f5519693d8f79
PriyankaManna <priyanka.manna@bacancy.com>
2022-02-09 12:55:43 +0530

    Day 1 Task

---
+++ Day-1/task1.rb
@@ -0,0 +1,14 @@
+# puts method to output string 
+puts "Priyanka Manna"
+puts "print string"
+
+# p method is used generally to identify error . it outputs string with double qoutes
+p 'Ruby on Rails Tutorials'
+p 'Day 1 task'
+
+#print does not have newline by default 
+print 'printing using print'
+print 'some new string '
+
+putc 0xA #hexadecimal code for the newline character.
+putc 'Ruby' #only single character is print
